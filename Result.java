7bd80fdccb2eaa2b26cf32fa8592d73589cacf6a
naseebapk <50262470+naseebapk@users.noreply.github.com>
2020-06-06 12:44:21 +0530

    Add files via upload

--- Result.java
+++ Result.java
@@ -8,9 +8,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
 
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.MediaType;
@@ -26,7 +23,6 @@ import java.io.IOException;
 
 public class Result extends AppCompatActivity {
 EditText mio,temp,rain,hum;
-//RadioButton h2,h3,h4,i5,i6,i7,s8,s9,s10;
 Button res,view;
 String humid,illumi,inspe;
     @Override
@@ -34,27 +30,10 @@ String humid,illumi,inspe;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        mio=(EditText)findViewById(R.id.editText1);
-        temp=(EditText)findViewById(R.id.editText2);
-        rain=(EditText)findViewById(R.id.editText5);
-        hum=(EditText)findViewById(R.id.editText6);
-
-
-//        h2=(RadioButton)findViewById(R.id.radioButton2);
-//        h3=(RadioButton)findViewById(R.id.radioButton3);
-//        h4=(RadioButton)findViewById(R.id.radioButton4);
-//
-//
-//        i5=(RadioButton)findViewById(R.id.radioButton5);
-//        i6=(RadioButton)findViewById(R.id.radioButton6);
-//        i7=(RadioButton)findViewById(R.id.radioButton7);
-//
-//
-//        s8=(RadioButton)findViewById(R.id.radioButton8);
-//        s9=(RadioButton)findViewById(R.id.radioButton9);
-//        s10=(RadioButton)findViewById(R.id.radioButton10);
-
-
+        temp=(EditText)findViewById(R.id.temp);
+        mio=(EditText)findViewById(R.id.moi);
+        rain=(EditText)findViewById(R.id.rain);
+        hum=(EditText)findViewById(R.id.hum);
         res=(Button)findViewById(R.id.button5);
         view=(Button)findViewById(R.id.button3);
 
@@ -79,26 +58,6 @@ String humid,illumi,inspe;
     public  void reg_db()
     {
 
-//        if(h2.isChecked())
-//            humid="3";
-//        else if(h3.isChecked())
-//            humid="2";
-//        else
-//            humid="1";
-//
-//        if(i5.isChecked())
-//            illumi="3";
-//        else if(i6.isChecked())
-//            illumi="2";
-//        else
-//            illumi="1";
-//
-//        if(s8.isChecked())
-//            inspe="3";
-//        else if(s9.isChecked())
-//            inspe="2";
-//        else
-//            inspe="1";
 
         MediaType MEDIA_TYPE = MediaType.parse("application/json");
         String url = "http://192.168.43.79:8000/result/android/";
@@ -110,11 +69,6 @@ String humid,illumi,inspe;
                     postdata.put("mio", mio.getText().toString());
                     postdata.put("rain", rain.getText().toString());
                     postdata.put("hum", hum.getText().toString());
-
-//                    postdata.put("humid", humid);
-//                    postdata.put("illumi", illumi);
-//                    postdata.put("inspe", inspe);
-
                     postdata.put("uid",Login.uid);
 
 
